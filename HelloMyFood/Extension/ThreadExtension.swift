51b6f18722ab809e064e0e47c2548d88646c2466
League2EB <poloa51404@gmail.com>
2021-07-15 13:27:43 +0800

    [RM] 移除沒有使用的Code

--- HelloMyFood/Extension/ThreadExtension.swift
+++ HelloMyFood/Extension/ThreadExtension.swift
@@ -5,26 +5,6 @@
 //  Created by Lazy on 2021/7/15.
 //
 
-public func onBackgroundThread(block: @escaping () -> Void) {
-    DispatchQueue.global(qos: .background).async(execute: block)
-}
-
-public func onBackgroundThreadSync(block: () -> Void) {
-    DispatchQueue.global(qos: .background).sync(execute: block)
-}
-
 public func onMainThread(block: @escaping () -> Void) {
     DispatchQueue.main.async(execute: block)
-}
-
-public func onMainThreadSync(block: () -> Void) {
-    DispatchQueue.main.sync(execute: block)
-}
-
-public func createQueue(_ queueLabel: String) -> DispatchQueue {
-    return DispatchQueue(label: queueLabel, qos: .utility, attributes: [.concurrent])
-}
-
-public func onBackgroundThreadReturn(_ queueLabel: String) -> DispatchQueue {
-    return DispatchQueue(label: queueLabel, qos: .background)
 }
