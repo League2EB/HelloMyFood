f74e1599cd4a40cb41bc36c0a880a89068aba3b1
League2EB <poloa51404@gmail.com>
2021-07-15 21:01:09 +0800

    [MOD] 優化LogTextView內容邊距

--- HelloMyFood/View/HMFMainViews.swift
+++ HelloMyFood/View/HMFMainViews.swift
@@ -10,7 +10,11 @@ class HMFMainViews: NSObject {
     /// 數字輸入框
     @IBOutlet weak var countTextField: UITextField!
     /// Log顯示畫面
-    @IBOutlet weak var consolLogView: UITextView!
+    @IBOutlet weak var consolLogView: UITextView! {
+        didSet {
+            consolLogView.contentInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        }
+    }
     /// 隨機產生1-20隨機數字按鈕
     @IBOutlet weak var randomBtn: HMFRoundCornersButton!
     /// 產生按鈕
